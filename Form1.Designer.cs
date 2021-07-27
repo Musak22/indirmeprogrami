58f78c11170d2f6f0d3bebd6e097e34fc72eac4a
Musak22 <beratucar@outlook.com>
2021-07-27 15:29:12 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btndownload = new System.Windows.Forms.Button();
+            this.lblperc = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnlist = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.txtbxselecteditem = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Enabled = false;
+            this.progressBar1.Location = new System.Drawing.Point(273, 375);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(328, 46);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // btndownload
+            // 
+            this.btndownload.Location = new System.Drawing.Point(436, 291);
+            this.btndownload.Name = "btndownload";
+            this.btndownload.Size = new System.Drawing.Size(139, 35);
+            this.btndownload.TabIndex = 1;
+            this.btndownload.Text = "indir";
+            this.btndownload.UseVisualStyleBackColor = true;
+            this.btndownload.Click += new System.EventHandler(this.btnindir_Click);
+            // 
+            // lblperc
+            // 
+            this.lblperc.AutoSize = true;
+            this.lblperc.Location = new System.Drawing.Point(337, 408);
+            this.lblperc.Name = "lblperc";
+            this.lblperc.Size = new System.Drawing.Size(0, 13);
+            this.lblperc.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Enabled = false;
+            this.label1.Location = new System.Drawing.Point(270, 428);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "İndirilen :";
+            // 
+            // btnlist
+            // 
+            this.btnlist.Location = new System.Drawing.Point(273, 213);
+            this.btnlist.Name = "btnlist";
+            this.btnlist.Size = new System.Drawing.Size(139, 35);
+            this.btnlist.TabIndex = 5;
+            this.btnlist.Text = "listele";
+            this.btnlist.UseVisualStyleBackColor = true;
+            this.btnlist.Click += new System.EventHandler(this.btnlist_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 22);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(767, 140);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // txtbxselecteditem
+            // 
+            this.txtbxselecteditem.Location = new System.Drawing.Point(273, 178);
+            this.txtbxselecteditem.Name = "txtbxselecteditem";
+            this.txtbxselecteditem.Size = new System.Drawing.Size(479, 20);
+            this.txtbxselecteditem.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.fsdlogo;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 178);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(242, 70);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(273, 291);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 35);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "farklı kaydet";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(273, 255);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(479, 20);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 264);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(242, 173);
+            this.listBox1.TabIndex = 11;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(789, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtbxselecteditem);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnlist);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblperc);
+            this.Controls.Add(this.btndownload);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btndownload;
+        private System.Windows.Forms.Label lblperc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnlist;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox txtbxselecteditem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+            private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 1)
+            {
+                var item = listView1.SelectedItems[0].Text;
+                txtbxselecteditem.Text = item;
+ }
+        }
+
+        private void btnindir_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedIndices.Count<=0)
+            {
+                MessageBox.Show("Please select file for download");
+            }
+            else
+            {
+                System.IO.Directory.CreateDirectory(txtbxselecteditem.Text + @"\yeni_klasor");
+
+                if (System.IO.File.Exists(textBox1.Text+ "\\"+ txtbxselecteditem.Text) == false)
+                {
+                    foreach (ListViewItem item in listView1.SelectedItems)
+                    {
+
+
+                        try
+                        {
+
+                  
+                        using (WebClient wc = new WebClient())
+                        {
+                            wc.DownloadProgressChanged += Wc_DownloadProgressChanged;
+                            wc.DownloadFile(
+                                // Param1 = Link of file
+                                new System.Uri("http://kurulum.fsdyazilim.com/FsdOrderv21/" + item.Text),
+                                // Param2 = Path to save
+                                textBox1.Text + "\\" + item.Text
+                            ); 
+                        
+                            }
+
+                            listBox1.Items.Add(item.Text);
+                        }
+                        catch (Exception err)
+                        {
+                            MessageBox.Show(err.Message);
+
+                        }
+                        //MessageBox.Show("Dosya başarıyla indirildi ✓ ");
+                    }
+                }
+
+                else
+                {
+                    MessageBox.Show("indirme başlatılamadı , dosya zaten mevcut");
+                }
+            }
+        }
+
+        private void Wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            lblperc.Text = "%"+ e.ProgressPercentage.ToString();
+        }
+
+        private void btnlist_Click(object sender, EventArgs e)
+        {
+
+
+            string uri = "http://kurulum.fsdyazilim.com/FsdOrderv21/OrderFileList.txt";
+            WebRequest request = WebRequest.Create(uri);
+            WebResponse response = request.GetResponse();
+            //Regex regex = new Regex("\r\n|\r|\n");
+
+            using (var reader = new StreamReader(response.GetResponseStream()))
+            {
+                string[] result = reader.ReadToEnd().Split(new Char[] {'\r','\n' });
+
+                //MatchCollection matches = regex.Matches(result);
+                //if (matches.Count == 0)
+                //{
+
+                //    return;
+                //}
+
+                //foreach (Match match in matches)
+                ////{
+                //    if (!match.Success) { continue; }
+                //    string name = match.Groups["name"].Value;
+                
+                foreach (string item in result)
+                {
+                    if (item.Trim().Length > 0     ) 
+                    {
+                           listView1.Items.Add(item);
+
+
+                    }
+   
+                }
+         
+                //}
+
+
+            }
+
+            btndownload.Enabled = true;
+            lblperc.Enabled = true;
+            label1.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = true;
+            // Show the FolderBrowserDialog.  
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBox1.Text = folderDlg.SelectedPath;
+                Environment.SpecialFolder root = folderDlg.RootFolder;
+            }
+    }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
+
